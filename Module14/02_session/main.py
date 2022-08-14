86f487700686f4139e7a388559907f5bf0eb8e51
Alexandr Bykov <Bykov.alexandr95@yandex.ru>
2022-08-14 13:58:45 +0300

    Скорректировал задачу, добавив условия

--- Module14/02_session/main.py
+++ Module14/02_session/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+
+print("Введите первую точку")
+x1 = float(input('X: '))
+y1 = float(input('Y: '))
+print("\nВведите вторую точку")
+x2 = float(input('X: '))
+y2 = float(input('Y: '))
+
+x_diff = x1 - x2
+y_diff = y1 - y2
+if x_diff == 0:
+    x_diff = x1
+    print(f'x == {x1}')
+elif y_diff == 0:
+    y_diff = y1
+    print(f'y == {y1}')
+else:
+    k = y_diff / x_diff
+    b = y2 - k * x2
+    print("Уравнение прямой, проходящей через эти точки:")
+    print("y = ", k, " * x + ", b)
