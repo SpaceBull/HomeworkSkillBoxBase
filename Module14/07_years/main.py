9c965e264c4259f59645790ee411f57e94a8be5d
Alexandr Bykov <Bykov.alexandr95@yandex.ru>
2022-08-16 22:52:33 +0300

    tasks solved

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+def count_special_dates(one_number, two_number):
+    for year in range(one_number, two_number + 1):
+        number_one = year // 1000
+        number_two = year // 100 % 10
+        number_three = year // 10 % 10
+        number_four = year % 10
+        if number_one == number_two == number_three \
+                or number_two == number_three == number_four \
+                or number_three == number_four == number_one \
+                or number_one == number_two == number_four:
+            print(year)
+
+
+first_number = int(input('Введите первый год: '))
+second_number = int(input('Введите второй год: '))
+print(f'Года от {first_number} до {second_number} с тремя одинаковыми цифрами:')
+count_special_dates(first_number, second_number)
+
+# def count_special_dates(a, b):
+# for year in range(a, b + 1):
+# for symbol in str(year): #Изначально мыслил в эту сторону, но присваивая
+# symbol - 0 ,то year не делит на symbol (так как на ноль делить нельзя)
