2604e101c51a1da045f6c4298dcdfa01c5632bde
Alexandr Bykov <Bykov.alexandr95@yandex.ru>
2022-09-11 19:10:17 +0300

    'optimized_the_counting_process'

--- Module16/04_party/main.py
+++ Module16/04_party/main.py
@@ -1,3 +1,23 @@
 guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
 
-# TODO здесь писать код
+while True:
+    print(f'Сейчас на вечеринке {len(guests)} человек: {guests}')
+    print('Гость пришёл или ушёл?')
+    action = input('')
+    if action == 'пришёл':
+        name_new_guest = input('Имя гостя: ')
+        if len(guests) < 6:
+            guests.append(name_new_guest)
+            print(f'Привет, {name_new_guest}!')
+        else:
+            print(f'Прости, {name_new_guest}, но мест нет.')
+    if action == 'ушёл':
+        name_guest = input('Имя гостя: ')
+        if name_guest not in guests:
+            print('Такого гостя нет!')
+        else:
+            guests.remove(name_guest)
+            print(f'Пока, {name_guest}')
+    if action == 'Пора спать':
+        print('Вечеринка закончилась, все легли спать.')
+        break
