78f8f5b41e568f9b0fe37eb66b68cdadf28c5a8b
Alexandr Bykov <Bykov.alexandr95@yandex.ru>
2022-09-11 18:20:02 +0300

    16.2 Отсортировал 2 списка в один по возрастанию

--- Module16/02_line/main.py
+++ Module16/02_line/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+first_rank = list(range(160, 176 + 1, 2))
+second_rank = list(range(162, 180 + 1, 3))
+
+new_rank = []
+new_rank.extend(first_rank)
+new_rank.extend(second_rank)
+new_rank.sort(reverse=False)
+print(f'Отсортированный список учеников: {new_rank}')
+
