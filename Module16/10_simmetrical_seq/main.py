fafe407f107495832396f9abb28ecf5a1af87122
Alexandr Bykov <Bykov.alexandr95@yandex.ru>
2022-09-11 19:36:17 +0300

    looking_for_a_parallelogram_of_numbers

--- Module16/10_simmetrical_seq/main.py
+++ Module16/10_simmetrical_seq/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+numbers = int(input('Кол-во чисел: '))
+roster = []
+new_roster = []
+for _ in range(numbers):
+    number = int(input('Введите число: '))
+    roster.append(number)
+if roster == roster[::-1]:
+    print('Цифры симметричны')
+if roster != roster[::-1]:
+    if roster[-1] == roster[-2]:
+        for i in range(len(roster), 2, -1):
+            new_roster.append(roster[-i])
+    else:
+        for i in range(len(roster), 1, -1):
+            new_roster.append(roster[-i])
+    print(f'Последовательность {roster}')
+    print(f'Нужно приписать чисел: {len(new_roster)}')
+    print(f'Сами числа: {new_roster[::-1]}')
+
