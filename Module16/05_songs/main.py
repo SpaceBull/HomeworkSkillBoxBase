9f2e9daeb3902deffb98465439242b206eb8933c
Alexandr Bykov <Bykov.alexandr95@yandex.ru>
2022-09-11 19:24:06 +0300

    Counts_the_number_of_minutes_in_the_playlist

--- Module16/05_songs/main.py
+++ Module16/05_songs/main.py
@@ -10,4 +10,15 @@ violator_songs = [
     ['Clean', 5.83]
 ]
 
-# TODO здесь писать код
+time_song = 0
+number_of_songs = int(input('Введите кол-во песен: '))
+for number in range(1, number_of_songs + 1):
+    print(f'Название {number}-й песни: ')
+    song = input('')
+    for i_song in violator_songs:
+        if i_song[0] == song:
+            time_song += i_song[1]
+
+time_song = (round(time_song, 2))
+print(f'Общее время звучания песен: {time_song} минуты')
+
