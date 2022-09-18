5ca2a5d4876a85c929cf64771e3779803469afaf
Alexandr Bykov <Bykov.alexandr95@yandex.ru>
2022-09-18 16:31:04 +0300

    Заполняет реестр победителей

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+import random
+
+first_command = [round(random.uniform(5, 10), 2) for _ in range(20)]
+second_command = [round(random.uniform(5, 10), 2) for _ in range(20)]
+
+winners = [first_command[index]
+           if first_command[index] > second_command[index]
+           else second_command[index]
+           for index in range(20)]
+
+print(f'first command: {first_command}')
+print(f'second command: {second_command}')
+print(f'Tour Winners:{winners}')
