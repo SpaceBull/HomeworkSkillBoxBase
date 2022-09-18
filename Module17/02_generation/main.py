88c0a1d212230072da83e285003d1f102090dbb3
Alexandr Bykov <Bykov.alexandr95@yandex.ru>
2022-09-18 15:29:23 +0300

    Замена чисел в сгенерированном списке

--- Module17/02_generation/main.py
+++ Module17/02_generation/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+import random
+number = int(input('Enter the number: '))
+answer = [random.randint(1, 20) for random_number in range(number)]
+new_answer = [1 if x % 2 == 0 else answer[x] % 5 for x in range(len(answer))]
+
+print('Result: ', answer)
+print('New Result: ', new_answer)
+
+
+
