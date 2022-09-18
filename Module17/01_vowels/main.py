99f92916607261db787b98807e45ecdc876df510
Alexandr Bykov <Bykov.alexandr95@yandex.ru>
2022-09-18 13:41:54 +0300

    Ищет гласные в тексте

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+text = input('Enter the text: ')
+vowel_letters = 'ёйуеыаоэяию'
+
+new_roster_vowel_letters = [letter for letter in text
+                            for symbol in vowel_letters
+                            if letter == symbol]
+
+print(f'roster vowel letters: {new_roster_vowel_letters}')
+print(f'roster length: {len(new_roster_vowel_letters)}')
+
