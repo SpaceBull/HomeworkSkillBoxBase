d749a5c6ebd69193d279b692b3ed3017cfd6c762
Alexandr Bykov <Bykov.alexandr95@yandex.ru>
2022-09-23 19:52:03 +0300

    в сгенерируемом списке двигает нули в конец массива

--- Module17/06_list_compression/main.py
+++ Module17/06_list_compression/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+import random
+number = int(input('Количество чисел в списке:  '))
+roster_numbers = [random.randint(0, 2) for _ in range(number)]
+print(f'Список до сжатия: {roster_numbers}')
+
+num_one = [num for num in roster_numbers if num > 0]
+print(f'Список после сжатия: {num_one}')
+
+
+# num_nol = [num for num in roster_numbers if num == 0]
+# num_one.extend(num_nol)
+# print(f'Отсортированный список: {num_one}')
