f5e017b5214d3091f4824b5bd518920e6a29dbf1
Alexandr Bykov <Bykov.alexandr95@yandex.ru>
2022-09-25 18:08:46 +0300

    Шифрует сообщения по методу Цезаря

--- Module17/10_caesar_cipher/main.py
+++ Module17/10_caesar_cipher/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+alphabet = list(alphabet)
+new_text = []
+text = input('Введите сообщение: ')
+shift = int(input('Введите сдвиг: '))
+
+for symbol in text:
+    if symbol not in list(alphabet):
+        new_text.append(symbol)
+        continue
+    index = alphabet.index(symbol)
+    index_symbol = index + shift
+    if index_symbol >= 32:
+        index_symbol = 32 - index_symbol + 1
+        new_symbol = alphabet[index_symbol]
+        new_text.append(new_symbol)
+    else:
+        new_symbol = alphabet[index_symbol]
+        new_text.append(new_symbol)
+
+result = ''.join(new_text)
+print(result)
+
