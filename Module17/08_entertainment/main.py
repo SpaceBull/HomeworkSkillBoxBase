531d94e9bc484c12e8f3369ecc3502c2adb37446
Alexandr Bykov <Bykov.alexandr95@yandex.ru>
2022-09-25 14:12:45 +0300

    программа показывает сбитые палки в игре

--- Module17/08_entertainment/main.py
+++ Module17/08_entertainment/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+import random
+
+number_of_sticks = int(input('Количество палок: '))
+sticks = ['|' for _ in range(number_of_sticks)]
+number_of_throws = int(input('Количество бросков: '))
+initial_number = 1
+
+for cast in range(number_of_throws):
+    left_border = random.randint(initial_number, number_of_sticks)
+    right_border = random.randint(left_border, number_of_sticks)
+    difference = right_border + 1 - left_border
+    sticks[left_border - 1: right_border] = '.' * difference
+    if right_border < number_of_sticks:
+        initial_number = right_border
+    elif right_border == number_of_sticks:
+        initial_number = 1
+        left_border = right_border
+    print(f'Бросок {cast + 1}. Сбиты палки с номера {left_border} по номер {right_border}.')
+
+print(f'Результат: {sticks}')
+
+
+
+
+
+
+
