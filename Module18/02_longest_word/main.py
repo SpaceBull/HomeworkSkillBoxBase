71ed46dfd54199a4c95b52ac0c33446930d14559
Alexandr Bykov <Bykov.alexandr95@yandex.ru>
2022-10-12 23:29:41 +0300

    Ищет длинное слово и считает буквы

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+word = input('Введите строку: ').split()
+number_of_letters = 1
+long_word = ''
+for text in word:
+    if number_of_letters <= len(text):
+        number_of_letters = len(text)
+        long_word = text
+
+print(f'Самое длинное слово: {long_word}')
+print(f'Длина этого слова: {len(long_word)}')
