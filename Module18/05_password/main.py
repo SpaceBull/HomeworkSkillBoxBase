35f6ed2145acc5b173c367e77656fb1204dbaf0a
Alexandr Bykov <Bykov.alexandr95@yandex.ru>
2022-10-13 10:27:35 +0300

    проверяет пароль на Верхний регистр и буквы

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+while True:
+    password = input('Придумайте пароль: ')
+    digits_in_the_password = len([number for number in password if number.isdigit()])
+    if len(password) > 8 and password.islower() != True and digits_in_the_password >= 3:
+        print('Это надёжный пароль!')
+        break
+    else:
+        print('Пароль ненадёжный. Попробуйте ещё раз.')
