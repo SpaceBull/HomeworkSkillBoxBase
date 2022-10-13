8aaf2b3460d43d1a3ee72f1bcdcbe2cc151f4144
Alexandr Bykov <Bykov.alexandr95@yandex.ru>
2022-10-13 10:39:39 +0300

    Кодирует строку с количеством букв

--- Module18/06_compression/main.py
+++ Module18/06_compression/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+text = input('Введите строку: ')
+text = [letter for letter in text]
+copy_text = text[:]
+copy_text += '!'
+index = -1
+roster_letters = []
+result_letters = []
+answer = []
+result = []
+
+for letter in text:
+    roster_letters.append(letter)
+    index += 1
+    if copy_text[index] != copy_text[index + 1]:
+        result_letters.append(roster_letters)
+        roster_letters = []
+
+for letter in result_letters:
+    str_letter = letter[0]
+    count_letter = str(len(letter))
+    answer = str_letter + count_letter
+    result += answer
+    result = ''.join(result)
+
+print(f'Закодированная строка: {result}')
