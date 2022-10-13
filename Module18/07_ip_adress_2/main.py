53fd89d5bfdab9de349bc83150138810d513cf06
Alexandr Bykov <Bykov.alexandr95@yandex.ru>
2022-10-13 11:30:58 +0300

    считает количество сдвигов у сдвинутой строки

--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -1,21 +1,20 @@
-ip_address = input('Введите IP: ')
-dot = ip_address.count('.')
-answer = False
-if dot == 3:
-    for number in ip_address.split('.'):
-        if not number.isdigit():
-            print(f'{number} — это не целое число.')
-            break
-        if int(number) > 255:
-            print(f'{number} превышает 255.')
-            break
-        if int(number) < 0:
-            print(f'{number} меньше 0.')
-            break
-        if number.isdigit():
-            answer = True
-else:
-    print('Адрес — это четыре числа, разделённые точками.')
+first_line = input('Первая строка: ')
+second_line = input('Вторая строка: ')
+count_letter = len(first_line)
+shift = 0
 
-if answer:
-    print('IP-адрес корректен.')
+first_roster = [letter for letter in first_line]
+second_roster = [letter for letter in second_line]
+
+while count_letter != 0:
+    shift += 1
+    letter = second_roster[len(second_roster) - 1]
+    copy_letter = letter
+    second_roster.remove(letter)
+    second_roster.insert(0, copy_letter)
+    count_letter -= 1
+    if second_roster == first_roster:
+        print(f'Первая строка получается из второй со сдвигом {shift}.')
+        break
+    if count_letter == 0:
+        print('Первую строку нельзя получить из второй с помощью циклического сдвига.')
