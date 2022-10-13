60d66882d002b85ff5ae6a74389ec9ad044cb51a
Alexandr Bykov <Bykov.alexandr95@yandex.ru>
2022-10-13 11:34:14 +0300

    Проверяет на целостность IP адреса

--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -1,17 +1,21 @@
-text = input('Сообщение: ').split()
-new_text = []
-for word in text:
-    if not word.isalpha():
-        symbol = word[len(word) - 1]
-        copy_symbol = symbol
-        temp_word = [letter for letter in word]
-        temp_word.remove(symbol)
-        temp_word = temp_word[::-1]
-        temp_word.insert(len(word) - 1, copy_symbol)
-        word = ''.join(temp_word)
-        new_text.append(word)
-    else:
-        new_text.append(word[::-1])
+ip_address = input('Введите IP: ')
+dot = ip_address.count('.')
+answer = False
+if dot == 3:
+    for number in ip_address.split('.'):
+        if not number.isdigit():
+            print(f'{number} — это не целое число.')
+            break
+        if int(number) > 255:
+            print(f'{number} превышает 255.')
+            break
+        if int(number) < 0:
+            print(f'{number} меньше 0.')
+            break
+        if number.isdigit():
+            answer = True
+else:
+    print('Адрес — это четыре числа, разделённые точками.')
 
-answer = ' '.join(new_text)
-print(f'Новое сообщение: {answer}')
+if answer:
+    print('IP-адрес корректен.')
