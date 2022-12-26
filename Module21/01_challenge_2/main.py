c9868a56c6ef3090ab84d8512562fa54f20d6295
Alexandr Bykov <Bykov.alexandr95@yandex.ru>
2022-12-26 22:20:40 +0300

    outputs_the_numbers_in_order

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+def outputs_the_numbers_in_order(first_number):
+    if first_number == 0:
+        return first_number
+    else:
+        outputs_the_numbers_in_order(first_number - 1)
+        print(first_number)
+
+
+number = int(input('Введите число:'))
+outputs_the_numbers_in_order(number)
