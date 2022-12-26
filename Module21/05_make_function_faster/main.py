007db4ba79a772c30455cf7a744f8189417b7e2a
Alexandr Bykov <Bykov.alexandr95@yandex.ru>
2022-12-26 22:53:06 +0300

    Speeding up the function

--- Module21/05_make_function_faster/main.py
+++ Module21/05_make_function_faster/main.py
@@ -1,9 +1,18 @@
-def calculating_math_func(data):
-    result = 1
-    for index in range(1, data + 1):
-        result *= index
-    result /= data ** 3
-    result = result ** 10
-    return result
+def calculating_math_func(data, second_data, base=dict()):
+    if second_data not in base:
+        if data < second_data:
+            for index in range(data, second_data + 1):
+                data *= index
+                base.update({index: (data / second_data ** 3) ** 10})
+            data = second_data
+            return base, data
+    else:
+        return [base[i] for i in range(data, second_data + 1)]
 
-# TODO оптимизировать функцию
+
+while True:
+    first_number = int(input('Введите начальное число: '))
+    finish_number = int(input('Введите конечное число: '))
+    answer = calculating_math_func(first_number, finish_number)
+    print(answer)
+    
