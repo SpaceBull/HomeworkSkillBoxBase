0d286918e880861d2ed8f57b0111eb9538c3a02c
Alexandr Bykov <Bykov.alexandr95@yandex.ru>
2022-12-26 22:22:25 +0300

    do_zip_two

--- Module21/02_my_zip_2/main.py
+++ Module21/02_my_zip_2/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+def do_zip_two(first_data, second_data):
+    result = ((first_data[index], second_data[index]) for index in range(min(len(first_data), len(second_data))))
+    return result
+
+
+answer = do_zip_two(first_data=[{'h': 9}, 'b', 'v', 'u'], second_data=(30, {40, }, [90], 'p'))
+print(answer) # Если раскрыть генератор, то-> [({'h': 9}, 30), ('b', {40}), ('v', [90]), ('u', 'p')]
