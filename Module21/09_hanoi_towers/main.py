6deeff487dc60c2f443dce82260b7a309f086c72
Alexandr Bykov <Bykov.alexandr95@yandex.ru>
2022-12-27 00:01:09 +0300

    the_hanoi_towers_algorithm

--- Module21/09_hanoi_towers/main.py
+++ Module21/09_hanoi_towers/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def the_hanoi_towers_algorithm(disks, start, finish):
+    if disks <= 0:
+        return
+    temp = 6 - start - finish
+    the_hanoi_towers_algorithm(disks - 1, start, temp)
+    print(f'Переложить диск {disks} со стержня номер {start} на стержень номер {finish}')
+    the_hanoi_towers_algorithm(disks - 1, temp, finish)
+
+
+number_of_disks = int(input('Введите количество дисков: '))
+the_hanoi_towers_algorithm(number_of_disks, 1, 3)
