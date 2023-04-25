64e087e783285ea5b188a5fdd70a47425683543e
Alex <Bykov.alexandr95@yandex.ru>
2023-04-25 22:01:30 +0300

    Solved problems with the use of decorators

--- Module27/01_how_are_you/main.py
+++ Module27/01_how_are_you/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+from typing import Callable, Any
+import functools
+
+
+def how_are_you(func: Callable) -> Callable:
+    """
+    Декоратор how_are_you. Спрашивает "как дела?" и отвечает.
+    """
+    @functools.wraps(func)
+    def wrapped_question() -> Any:
+        input('Как дела? ')
+        print('А у меня не очень! Ладно, держи свою функцию.')
+        return func()
+    return wrapped_question
+
+
+@how_are_you
+def test():
+    print('<Тут что-то происходит...>')
+
+
+test()
+print(how_are_you.__doc__)
