8d3186181033f8f5a98b2b7e1832a5e7d44e9166
Alex <Bykov.alexandr95@yandex.ru>
2023-05-03 16:29:03 +0300

    Solved problems with the help of new knowledge

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,35 @@
-# TODO здесь писать код
+class File:
+    """
+    Класс "Контекст менеджер" File - Открывает файл и проверяет на наличие уже существующего.
+        Attributes:
+        filename - Наименование файла
+        mode - Режим чтения/записи (r/w)
+    """
+    def __init__(self, filename: str, mode: str) -> None:
+        print('Открывание файла')
+        self.filename = filename
+        self.mode = mode
+
+    def __enter__(self) -> 'File':
+        """
+        Метод Enter - открывая в обработке файл с заданными атрибутами, если файл уже существует,
+         то откроет в режим записи.
+        :return: возвращает в глобальную переменную.
+        """
+        try:
+            self.file = open(self.filename, self.mode, encoding='utf-8')
+        except FileNotFoundError:
+            self.file = open(self.filename, 'w', encoding='utf-8')
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        Метод Exit - закрывает в завершение файл.
+        :return: Возвращая True, подтверждая успешное завершение.
+        """
+        self.file.close()
+        return True
+
+
+with File('example.txt', 'r') as file:
+    file.write('Всем привет!')
