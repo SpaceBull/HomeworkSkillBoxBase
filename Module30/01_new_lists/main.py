52b9c2ac826affdaaa8daf2a6b2bb56cf2a480fc
Alex <Bykov.alexandr95@yandex.ru>
2023-05-27 13:57:53 +0300

    functional programming using lambda

--- Module30/01_new_lists/main.py
+++ Module30/01_new_lists/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+from typing import List
+from functools import reduce
+
+
+floats: List[float] = [12.3554, 4.02, 5.777, 2.12, 3.13, 4.44, 11.0001]
+names: List[str] = ["Vanes", "Alen", "Jana", "William", "Richards", "Joy"]
+numbers: List[int] = [22, 33, 10, 6894, 11, 2, 1]
+
+if __name__ == "__main__":
+    result_float = list(map(lambda x: round(x**3, 3), floats))
+    print(result_float)
+    result_names = list(filter(lambda x: len(x) > 4, names))
+    print(result_names)
+    result_numbers = reduce(lambda x, y: x * y, numbers)
+    print(result_numbers)
